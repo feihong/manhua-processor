e09b4045c4aa0ffb73a0eea85a122cde6b53067c
Feihong Hsu <feihong.hsu@gmail.com>
2022-12-23 20:29:05 -0600

    Define get_comics_table directly

--- cbz.ml
+++ cbz.ml
@@ -1,36 +1,39 @@
 (* Convert SQLite database file to a bunch of .cbz files *)
 
-type episode = {
-  id : string;
-  title : string;
-  short_title : string;
-  link : string option; [@yojson.option]
-}
+type input_episode = { path : string; title : string; short_title : string }
 [@@deriving yojson]
 
-type comic = { id : string; title : string; ep_list : episode list }
+type comic = { id : string; title : string; ep_list : input_episode list }
 [@@deriving yojson]
+
+type episode = { id : string; title : string; link : string }
 
 let get_data (json : Yojson.Safe.t) =
   match json with `Assoc lst -> List.assoc_opt "data" lst | _ -> None
 
-let get_comics (db : Sqlite3.db) =
+let get_comics_table (db : Sqlite3.db) =
   let statement =
     Sqlite3.prepare db "select data from dump where url like '%ComicDetail%'"
   in
-  Seq.unfold
-    (fun () ->
-      match Sqlite3.step statement with
-      | OK ->
-          Sqlite3.column_blob statement 0
-          |> Yojson.Safe.from_string |> get_data
-          |> Option.map (fun data -> (data |> comic_of_yojson, ()))
-      | _ -> None)
-    ()
+  let table = Hashtbl.create 10 in
+  let _code, () =
+    Sqlite3.fold statement
+      ~f:(fun () array ->
+        match array with
+        | [| BLOB data |] ->
+            data |> Yojson.Safe.from_string |> get_data
+            |> Option.map comic_of_yojson
+            |> Option.iter (fun (comic : comic) ->
+                   Hashtbl.add table comic.id comic)
+        | _ -> ())
+      ~init:()
+  in
+  table
 
 let () =
   print_endline "Inside incomplete cbz program";
   let db = Sqlite3.db_open Shared.db_file in
-  get_comics db |> Seq.iter (fun comic -> print_endline comic.title);
+  get_comics_table db |> Hashtbl.to_seq_values
+  |> Seq.iter (fun (comic : comic) -> print_endline comic.title);
   let _success = Sqlite3.db_close db in
   ()
