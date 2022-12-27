8fbf27c8cea212aeff0531cfc5c7dc9fcbf35a35
Feihong Hsu <feihong.hsu@gmail.com>
2022-12-26 18:38:36 -0600

    cbz is finally working!

--- cbz.ml
+++ cbz.ml
@@ -1,117 +1,131 @@
 (* Convert SQLite database file to a bunch of .cbz files *)
+open Printf
 
-type image = { path : string } [@@deriving yojson]
+type episode = { id : int; title : string; short_title : string }
+[@@deriving yojson] [@@yojson.allow_extra_fields]
 
-type input_episode = {
-  id : int;
-  path : string;
-  title : string;
-  short_title : string;
-  images : image list;
-}
-[@@deriving yojson]
+type comic = { id : int; title : string; ep_list : episode list }
+[@@deriving yojson] [@@yojson.allow_extra_fields]
 
-type comic = { id : int; title : string; ep_list : input_episode list }
-[@@deriving yojson]
+type image = { path : string } [@@deriving yojson] [@@yojson.allow_extra_fields]
 
-type episode = { id : int; title : string; link : string; images : string list }
+type imageIndex = { path : string; images : image list }
+[@@deriving yojson] [@@yojson.allow_extra_fields]
+
+type cbz = { title : string; link : string; images : string list }
 
 let query_to_json_seq db query =
   let statement = Sqlite3.prepare db query in
   Seq.unfold
     (fun () ->
       match Sqlite3.step statement with
-      | OK -> (
+      | ROW -> (
           match Sqlite3.column_blob statement 0 |> Yojson.Safe.from_string with
           | `Assoc lst -> Some (List.assoc "data" lst, ())
           | _ -> None)
-      | _ -> None)
+      | DONE -> None
+      | code ->
+          printf {|Unexpected return code for query "%s": %s\n|} query
+            (code |> Sqlite3.Rc.to_string);
+          None)
     ()
+
+let _dump_json json =
+  let oc = open_out "temp.json" in
+  output_string oc (json |> Yojson.Safe.to_string);
+  close_out oc
 
 let make_comics_table (db : Sqlite3.db) =
   query_to_json_seq db "select data from dump where url like '%ComicDetail%'"
   |> Seq.map (fun json ->
          let comic = comic_of_yojson json in
+         printf "Comic: %s\n" comic.title;
          (comic.id, comic))
   |> Hashtbl.of_seq
 
-let make_episode ~(comics_table : (int, comic) Hashtbl.t)
-    (input_episode : input_episode) =
+let make_cbz ~(comics_table : (int, comic) Hashtbl.t) (imageIndex : imageIndex)
+    =
   let re = Str.regexp {|/bfs/manga/\([0-9]+\)/\([0-9]+\)|} in
-  let path = input_episode.path in
+  let path = imageIndex.path in
   (* If path doesn't match, then it probably hasn't been bought *)
   match Str.string_match re path 0 with
-  | false -> None
+  | false ->
+      printf "The path %s did not match regexp" path;
+      None
   | true ->
       let comic_id = Str.matched_group 1 path |> int_of_string in
       let episode_id = Str.matched_group 2 path |> int_of_string in
       let comic = Hashtbl.find comics_table comic_id in
-      let episode : input_episode =
-        comic.ep_list
-        |> List.find (fun (ep : input_episode) -> ep.id = episode_id)
+      let episode : episode =
+        comic.ep_list |> List.find (fun (ep : episode) -> ep.id = episode_id)
       in
       Some
         {
-          id = episode_id;
           link =
-            Printf.sprintf "https://manga.bilibili.com/mc%i/%i" comic_id
-              episode_id;
+            sprintf "https://manga.bilibili.com/mc%i/%i" comic_id episode_id;
           title =
             [ comic.title; episode.short_title; episode.title ]
             |> List.filter_map (fun s ->
                    match String.trim s with "" -> None | s -> Some s)
             |> String.concat " ";
-          images = episode.images |> List.map (fun (im : image) -> im.path);
+          images = imageIndex.images |> List.map (fun (im : image) -> im.path);
         }
 
-let make_episodes_table (db : Sqlite3.db)
-    (comics_table : (int, comic) Hashtbl.t) =
+let make_imageIndex_table (db : Sqlite3.db) =
   query_to_json_seq db "select data from dump where url like '%GetImageIndex%'"
-  |> Seq.filter_map (fun json ->
-         json |> input_episode_of_yojson |> make_episode ~comics_table
-         |> Option.map (fun episode -> (episode.id, episode)))
+  |> Seq.map (fun json ->
+         let imageIndex = imageIndex_of_yojson json in
+         print_endline ("Image index: " ^ imageIndex.path);
+         (imageIndex.path, imageIndex))
   |> Hashtbl.of_seq
 
-let get_episode_images db (episode : episode) =
+let get_episode_images db (cbz : cbz) =
   let index = ref 0 in
-  episode.images |> List.to_seq
+  cbz.images |> List.to_seq
   |> Seq.filter_map @@ fun path ->
      incr index;
      let sql =
-       Printf.sprintf "select data from dump where url like '%s' LIMIT 1" path
+       sprintf "select data from dump where url like '%%%s%%' LIMIT 1" path
      in
      let statement = Sqlite3.prepare db sql in
      match Sqlite3.step statement with
-     | OK -> Some (Sqlite3.column_blob statement 0)
-     | _ ->
-         Printf.printf "No data found for image %i of %s" !index episode.title;
+     | ROW -> Some (Sqlite3.column_blob statement 0)
+     | code ->
+         printf "No data found for image %i of %s, code: %s\n" !index cbz.title
+           (code |> Sqlite3.Rc.to_string);
          None
 
-let make_cbz_file db (episode : episode) =
+let make_cbz_file db (cbz : cbz) =
   let image_count = ref 0 in
-  let comment = Printf.sprintf "%s\n%s" episode.title episode.link in
-  let filename1 = episode.title ^ ".cbz" in
-  let zf = Zip.open_out ~comment filename1 in
-  get_episode_images db episode
+  let comment = sprintf "%s\n%s" cbz.title cbz.link in
+  let expected_count = List.length cbz.images in
+  let init_filename = sprintf "%s (%i).cbz" cbz.title expected_count in
+  let zf = Zip.open_out ~comment init_filename in
+  get_episode_images db cbz
   |> Seq.iter (fun image ->
          incr image_count;
-         let filename = Printf.sprintf "%03i.jpg" !image_count in
-         Zip.add_entry image zf ~level:0 filename);
+         let image_filename = sprintf "%03i.jpg" !image_count in
+         (* printf "Image size: %i\n" (String.length image); *)
+         Zip.add_entry image zf ~level:0 image_filename);
   Zip.close_out zf;
 
-  (* Rename file to say how images are inside it *)
-  Sys.rename filename1 (Printf.sprintf "%s (%i).cbz" episode.title !image_count);
-
-  let expected_count = List.length episode.images in
-  if !image_count <> expected_count then
-    Printf.printf "Found %i images for %s, but expected %i" !image_count
-      episode.title expected_count
+  let final_filename =
+    if !image_count <> expected_count then (
+      printf "Found %i images for %s, but expected %i\n" !image_count cbz.title
+        expected_count;
+      let final_filename = sprintf "%s (%i).cbz" cbz.title !image_count in
+      (* Rename file to say how many actual images are inside it *)
+      Sys.rename init_filename final_filename;
+      final_filename)
+    else init_filename
+  in
+  print_endline ("Generated " ^ final_filename)
 
 let () =
-  print_endline "Inside incomplete cbz program";
   let db = Sqlite3.db_open Shared.db_file in
-  make_episodes_table db (make_comics_table db)
-  |> Hashtbl.to_seq_values
-  |> Seq.iter (make_cbz_file db);
+  let comics_table = make_comics_table db in
+  let imageIndexes = make_imageIndex_table db |> Hashtbl.to_seq_values in
+  let cbzs = imageIndexes |> Seq.filter_map (make_cbz ~comics_table) in
+  cbzs |> Seq.iter (make_cbz_file db);
   let _success = Sqlite3.db_close db in
   ()
